3f3671fc7aed84b87474df1501e6a22fbc2824bc
finsther <finsther@icloud.com>
2026-05-04 18:57:52 -0600

    Modify to add real connection to backend service

--- astro.config.ts
+++ astro.config.ts
@@ -1,15 +1,17 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
 
 export default defineConfig({
+  site: 'https://claktv.com',
   adapter: vercel({
     isr: {
       expiration: 1800,
     },
   }),
-  integrations: [icon()],
+  integrations: [icon(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
